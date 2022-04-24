40b4bcc1c7a45b6c0efe05c4d332d2c58aa5d93d
Marc Roig <marc12info@gmail.com>
2022-04-24 14:56:24 +0200

    🔨 Remove warning

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -4,12 +4,12 @@ function NavigationDots({ active }) {
   return (
     <div className='app__navigation'>
         {['home', 'about', 'work', 'skills', 'contact'].map((item, index) => 
-            <a 
+            <a
                 href={`#${item}`}
                 key={item+index}
                 className="app__navigation-dot"
                 style={active === item ? {backgroundColor: '#313BAC'} : {}}
-            />
+            > </a>
         )}
     </div>
   )
