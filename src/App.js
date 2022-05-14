91f122427498c166e90a2467ce1a3566fc8a8c9c
Marc Roig <marc12info@gmail.com>
2022-05-14 18:54:15 +0200

    ✨ Add react-scroll-snap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { About, Footer, Header, Skills, Work } from './container';
-
 import { Navbar } from './components';
-import React from 'react'
-
 import './App.scss';
 
+import useScrollSnap from 'react-use-scroll-snap';
+import React, { useRef } from 'react'
+
+
 const App = () => {
+
+  const scrollRef = useRef(null);
+  useScrollSnap({ ref: scrollRef, duration: 100, delay: 50 });
+
   return (
-    <div className="app">
+    <div className="app" ref={scrollRef}>
       <Navbar />
       <Header />
       <About />
